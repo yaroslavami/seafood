4f398e9f1f52724345ec7cbf6fa4fb8b7b329cc0
Yaroslava Mishchenko <yaroslavamishsce@gmail.com>
2023-08-12 00:08:17 -0400

    Update

---
+++ script/script.js
@@ -0,0 +1,7 @@
+var myhero = prompt('Good day my pretty hero, write your name down ', myhero);
+console.log('Name', myhero);
+var me = prompt('How would you like to call me? Write it ', me);
+console.log('Nickname', me);
+alert('Hello again my hero, now I have my own name, thank you!!');
+document.write('Hello I am your helper, ', me);
+document.write('. Nice to meet you, ', myhero)
