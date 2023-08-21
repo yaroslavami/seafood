e28043610d1df1a1b5518ff0de8558a32c2301d2
Yaroslava <81515499+yaroslavami@users.noreply.github.com>
2023-08-21 12:20:58 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -4,4 +4,4 @@ var me = prompt('How would you like to call me? Write it ', me);
 console.log('Nickname', me);
 alert('Hello again my hero, now I have my own name, thank you!!');
 document.write('Hello I am your helper, ', me);
-document.write('. Nice to meet you, ', myhero) }, 15000);
+document.write('. Nice to meet you, ', myhero) }, 10000);
