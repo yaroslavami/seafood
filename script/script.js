e8ff3bb9157cf3c14aebd405c1c2fa867976a03f
Yaroslava <81515499+yaroslavami@users.noreply.github.com>
2023-09-06 14:28:19 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,5 @@
-setTimeout(() => {var myhero = prompt('Good day my pretty hero, write your name down ', myhero);
+var myhero = prompt('Good day my pretty hero, write your name down ', myhero);
 console.log('Name', myhero);
 var me = prompt('How would you like to call me? Write it ', me);
 console.log('Nickname', me);
 alert('Hello again my hero, now I have my own name, thank you!!');
-alert('Hello I am your helper, ', me);
-alert('. Nice to meet you, ', myhero) }, 10000);
